57110703233bf7680aa4a4117af30fdc861e2110
Jahan S <jshahpro1@gmail.com>
2025-08-19 16:48:10 -0600

    move files to includes

--- includes/nav.php
+++ includes/nav.php
@@ -1,1 +1,11 @@
+<?php
+// nav.php - navigation menu
+?>
 
+<nav>
+    <ul>
+        <li><a href="index.php" class="active">Home</a></li>
+        <li><a href="about.php">About Vietnam</a></li>
+        <li><a href="festivals.php">Festivals</a></li>
+    </ul>
+</nav>
